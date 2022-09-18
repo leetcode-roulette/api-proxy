a8ed632b7f357e716c6cf5920799151a3a2a8b5b
Eric <eric.hicks.8989@gmail.com>
2022-09-17 21:00:26 -0700

    Move docs to root url - Remove url from `contact` in `swagger.json` - Set docs to the base url

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import { config } from 'dotenv';
 import express, { Application, NextFunction, Request, Response } from 'express';
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./docs/swagger.json";
 import v1Router from './v1/routes';
 import { Database } from './v1/db/db.config';
 import { PopulateProblems } from './v1/db/problems';
@@ -11,6 +13,7 @@ const serve = async () : Promise<void> => {
 
     await Database.connect();
 
+    app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
     app.use("/v1", v1Router);
 
     app.get('/', (req: Request, res: Response, next: NextFunction) => {
--- src/v1/routes/index.ts
+++ src/v1/routes/index.ts
@@ -1,12 +1,9 @@
 import express, { Router } from "express";
-import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "../docs/swagger.v1.json";
 import healthcheckRouter from "./healthcheck";
 import problemsRouter from "./problems";
 
 const router : Router = express.Router();
 
-router.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.use("/healthcheck", healthcheckRouter);
 router.use("/problems", problemsRouter);
 
