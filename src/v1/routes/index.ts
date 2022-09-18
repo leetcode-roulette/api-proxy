da75f8dcb71f60e41dcd7b02a377353a205a6bd2
Eric <eric.hicks.8989@gmail.com>
2022-09-17 20:49:50 -0700

    Add swagger docs for v1 api - Add `swagger-ui-express` to `package.json` - Create `swagger.v1.json` file - Add `/v1/docs` route for swagger docs

--- src/v1/routes/index.ts
+++ src/v1/routes/index.ts
@@ -1,9 +1,12 @@
 import express, { Router } from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../docs/swagger.v1.json";
 import healthcheckRouter from "./healthcheck";
 import problemsRouter from "./problems";
 
 const router : Router = express.Router();
 
+router.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.use("/healthcheck", healthcheckRouter);
 router.use("/problems", problemsRouter);
 
