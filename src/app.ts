f6fb6b6c05c8decb762100c23d38206bbbed968c
jared-ivory <26067982+Jared-Ivory@users.noreply.github.com>
2022-10-20 19:08:57 -0400

    Added cors

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
-import { config } from 'dotenv';
-import express, { Application } from 'express';
+import { config } from "dotenv";
+import express, { Application } from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./docs/swagger.json";
 import v1Router from "./v1/routes";
+import cors from "cors";
 
 config();
 
-export const app : Application = (() : Application => {
-    const app: Application = express();
-    app.use(express.json());
-    app.set('json spaces', 2)
-    app.use("/v1", v1Router);
-    app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+export const app: Application = ((): Application => {
+	const app: Application = express();
+	app.use(express.json());
+	app.use(cors());
+	app.set("json spaces", 2);
+	app.use("/v1", v1Router);
+	app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-    return app;
+	return app;
 })();
